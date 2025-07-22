fb7e158fffa8729aa01cf077f1e3b52141df7fbd
Stenny17 <stenny.rajkumar@gmail.com>
2025-07-22 19:51:48 +0530

    Update app.py with new changes

--- my-eks-app/src/app.py
+++ my-eks-app/src/app.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from EKS CI/CD Pipleline setup, Hurray!"}
+    return {"message": "Hello from EKS CI/CD Pipleline setup, Hurray, this should work now!!"}
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=False)
